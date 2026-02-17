16ff32b8b13a2e7bd02518f6497e4a42ef7d39af
Maltego000 <rarenewamd@gmail.com>
2026-02-18 00:42:50 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,193 @@
+CHANNEL_ID = -1002303837030 #канал для логов  
+
+CHANNELS = {
+    
+}
+
+private_channel_ids = {2486181193}
+
+VIP_PRICE = {
+    "USDT": 0.3,
+    "TON": 1.5,
+    "BTC": 0.0001,
+    "ETH": 0.001,
+    "BNB": 0.01,
+    "LTC": 0.02,
+    "DOGE": 50,
+    "TRX": 10,
+    "NOT": 2,
+}
+
+CURRENCY_PRICES = {
+    "1_day": {
+        "TON": 1.5,
+        "BTC": 0.0001,
+        "ETH": 0.001,
+        "USDT": 0.6,
+        "BNB": 0.01,
+        "LTC": 0.02,
+        "DOGE": 50,
+        "TRX": 10,
+        "NOT": 2,
+    },
+    "2_days": {
+        "TON": 2.5,
+        "BTC": 0.0002,
+        "ETH": 0.002,
+        "USDT": 3.0,
+        "BNB": 0.02,
+        "LTC": 0.03,
+        "DOGE": 75,
+        "TRX": 15,
+        "NOT": 3,
+    },
+    "5_days": {
+        "TON": 5.0,
+        "BTC": 0.0005,
+        "ETH": 0.005,
+        "USDT": 5.0,
+        "BNB": 0.05,
+        "LTC": 0.05,
+        "DOGE": 100,
+        "TRX": 20,
+        "NOT": 5,
+    },
+    "30_days": {
+        "TON": 10.0,
+        "BTC": 0.001,
+        "ETH": 0.01,
+        "USDT": 10.0,
+        "BNB": 0.1,
+        "LTC": 0.1,
+        "DOGE": 200,
+        "TRX": 30,
+        "NOT": 10,
+    },
+    "1_year": {
+        "TON": 50.0,
+        "BTC": 0.005,
+        "ETH": 0.05,
+        "USDT": 50.0,
+        "BNB": 0.5,
+        "LTC": 0.5,
+        "DOGE": 500,
+        "TRX": 100,
+        "NOT": 50,
+    },
+}
+
+SESSION_DIR = 'Session'
+bot_token = '8448907347:AAHjHhrUaDxhNRth-IBoGNVvAnIQvTHgqVk' 
+admin_chat_ids = ['8406972650'] 
+CRYPTO_PAY_TOKEN = '529565:AAKTvuMO0aAS9Wl0zuYnQv85eGPf5RQbMI9'
+
+senders = {
+    "miranovseverov@gmail.com": "kdbc vmdb djxf pmiq",
+    "alenaveterov@gmail.com": "hmiq xwmr yfmw prsa",
+    "ghostchat2023@gmail.com": "skch vagj pvmt dfst",
+    "borzghost15@gmail.com": "ogdd jnoq dqpa jstj",
+    "borzghost2023@gmail.com": "tcrq chtb ubhv bgjp",
+    "borzghost2024@gmail.com": "zdua vqac zspw dwzj",
+    "severovmiranov@gmail.com": "xvgc bwwc ytzq ydkv",
+    "wawilondurov@gmail.com": "qtrm btbi qoae vwvo",
+    "hujnadadada@gmail.com": "kjpw tmxw wurd cvrn",
+    "deaduser214@gmail.com": "gwvl rvjf thhc fzdt",
+    "volkuser87@gmail.com": "rtmx wzsv feqb vlwp",
+    "vertoletseverov@gmail.com": "cips ekel ulhm zmwp",
+    "severove05@gmail.com": "rpnn qkne ruoc ukcj",
+    "severove06@gmail.com": "wonq bnyv iavu ywmz",
+    "severove07@gmail.com": "miaf ydif jhgv egpt",
+    "severove520@gmail.com": "fvwg tuxe ytje kglp",
+    "vllyly8@gmail.com": "brar wdvm mlzl wqvq",
+    "sayzaldiarov@gmail.com": "yhbt blta yuhf wskh",
+    "argatovfolz@gmail.com": "hhoe uaqx mexp zqda",
+    "borzsch2024@gmail.com": "gdkp gcbx pxim ornq",
+    "severovsch@gmail.com": "mqoy oeqz jmal xlyf",
+    "severov2222222@gmail.com": "jrpg ciew fyhn oxyo",
+    "severov818@gmail.com": "kbnt iujk ioll buhi",
+    "severov236@gmail.com": "etas zzez imni behm",
+    "sairaolsen@yandex.ru": "tfeovoxateovicle",
+    "majawolf@yandex.ru": "ntsiwgqblppsxwwv",
+    "riosmag@yandex.ru": "yihsymfhnnuogbtf",
+    "dlatt6677@gmail.com": "usun ruef otzx zcrh",
+    "milapitts@yandex.com": "jnpipfzfmplxlkok",
+    "mamypokopants@yandex.com": "gorwijvdchyocnkj",
+    "mteteluopoe@yandex.com": "pfavacranfkklhtw",
+    "owenodling@yandex.com": "wmuajlzolphbnwfy",
+    "majawolf@yandex.com": "ntsiwgqblppsxwwv",
+    "faisalfry@yandex.com": "kxrsbcakgjoqnnlh",
+    "omerkelly@yandex.com": "zequgdjpjgqwokng",
+    "bluipantor@yandex.com": "eheldghouuuwgewo",
+    "ress.sedih@yandex.com": "yembmjmdcgtxwdjb",
+    "mvdsapfirskiy@gmail.com": "xqwu xznx jodo badz",
+    "sapfirskiymatvei@gmail.com": "bjqc mehb cgsk kovi",
+    "kutasikgabriel@gmail.com": "uvdi ipzk qetk otew"
+"magiskmodule6@gmail.com" "mhyb gpem qidd roii",
+"Karerras1345@gmail.com": "vukl zwmj zzkv njho",
+"Eden573519@gmail.com": "hson dlhh essl wltq",
+"whydll70sx@gmail.com": "tklx reiw uajj nbmj",
+"ermuvlu127@gmail.com": "hscq hctb acrv vtcp",
+"sliterhouse@gmail.com": "oozn flzm vfeu cpgt",
+'robiko3900@gmail.com': 'adyh vuit vnij errw',
+"nonames910s@gmail.com": "kmxl aeip kztp pzf",
+}
+
+receivers = [
+    'sms@telegram.org', 'dmca@telegram.org', 'abuse@telegram.org', 'sticker@telegram.org', 'support@telegram.org', 'support@telegram.org', 'dmca@telegram.org', 'security@telegram.org', 'sms@telegram.org', 'info@telegram.org', 'marta@telegram.org', 'spam@telegram.org', 'alex@telegram.org', 'abuse@telegram.org', 'pavel@telegram.org', 'durov@telegram.org', 'elies@telegram.org', 'ceo@telegram.org', 'mr@telegram.org', 'levlam@telegram.org', 'perekopsky@telegram.org', 'recover@telegram.org', 'germany@telegram.org', 'hyman@telegram.org', 'qa@telegram.org', 'Stickers@telegram.org', 'ir@telegram.org', 'vadim@telegram.org', 'shyam@telegram.org', 'stopca@telegram.org', '>support@telegram.org', 'ask@telegram.org', '125support@telegram.org', 'me@telegram.org', 'enquiries@telegram.org', 'api_support@telegram.org', 'marketing@telegram.org', 'ca@telegram.org', 'recovery@telegram.org', 'http@telegram.org', 'corp@telegram.org', 'corona@telegram.org', 'ton@telegram.org', 'sticker@telegram.org'
+]
+
+
+
+smtp_servers = {
+    "gmail.com": ("smtp.gmail.com", 587),
+    "yandex.ru": ("smtp.yandex.ru", 465),
+    "mail.ru": ("smtp.mail.ru", 465),
+    "rambler.ru": ("smtp.rambler.ru", 465),
+    "yahoo.com": ("smtp.mail.yahoo.com", 465),
+    "outlook.com": ("smtp.office365.com", 587),
+    "icloud.com": ("smtp.mail.me.com", 587),
+    "aol.com": ("smtp.aol.com", 587),
+    "zoho.com": ("smtp.zoho.com", 587),
+    "protonmail.com": ("smtp.protonmail.com", 587),
+    "t-online.de": ("secure.emailsrvr.com", 587),
+    "gmx.de": ("mail.gmx.com", 587),
+    "hotmail.de": ("smtp.live.com", 587),
+    "web.de": ("smtp.web.de", 587),
+    "gmx.net": ("mail.gmx.net", 587),
+    "posteo.de": ("posteo.de", 587),
+    "mailbox.org": ("smtp.mailbox.org", 587),
+    "1und1.de": ("smtp.1und1.de", 587),
+    "strato.de": ("smtp.strato.de", 465),
+    "fastmail.com": ("smtp.fastmail.com", 587),
+    "tutanota.com": ("smtp.tutanota.de", 587),
+    "runbox.com": ("smtp.runbox.com", 587),
+    "hushmail.com": ("smtp.hushmail.com", 587),
+    "countermail.com": ("smtp.countermail.com", 465),
+    "lavabit.com": ("smtp.lavabit.com", 465),
+    "cock.li": ("mail.cock.li", 587),
+    "migadu.com": ("smtp.migadu.com", 587),
+    "mailfence.com": ("smtp.mailfence.com", 587),
+    "kolabnow.com": ("smtp.kolabnow.com", 587),
+    "mailnesia.com": ("smtp.mailnesia.com", 587),
+    "mailcatch.com": ("smtp.mailcatch.com", 587),
+    "mintemail.com": ("smtp.mintemail.com", 587),
+    "spamgourmet.com": ("smtp.spamgourmet.com", 587),
+    "mytemp.email": ("smtp.mytemp.email", 587),
+    "temp-mail.org": ("smtp.temp-mail.org", 587),
+    "mailtemp.info": ("smtp.mailtemp.info", 587),
+    "fakemail.net": ("smtp.fakemail.net", 587),
+    "sharklasers.com": ("smtp.sharklasers.com", 587)
+}
+
+clients = [  
+    {"name": "Public MacOs Beta", "api_id": 2834, "api_hash": "68875f756c9b437a8b916ca3de215815"},
+    {"name": "Public Win Beta", "api_id": 2040, "api_hash": "b18441a1ff607e10a989891a5462e627"},
+    {"name": "Telegram Desktop", "api_id": 17349, "api_hash": "344583e45741c457fe1862106095a5eb"},
+    {"name": "Telegram X (Android)", "api_id": 21724, "api_hash": "3e0cb5efcd52300aec5994fdfc5bdc16"},
+    {"name": "web.telegram.org", "api_id": 2496, "api_hash": "8da85b0d5bfe62527e5b244c209159c3"},
+    {"name": "TDLib", "api_id": 94575, "api_hash": "a3406de8d171bb422bb6ddf3bbd800e2"},
+    {"name": "Telegram messenger CLI", "api_id": 2899, "api_hash": "36722c72256a24c1225de00eb6a1ca74"},
+    {"name": "Plus Messenger", "api_id": 16623, "api_hash": "8c9dbfe58437d1739540f5d53c72ae4b"},
+    {"name": "Telegram Swift", "api_id": 10840, "api_hash": "33c45224029d59cb3ad0c16134215aeb"},
+    {"name": "@XP_Diablo_XP", "api_id": 21826549, "api_hash": "c1a19f792cfd9e397200d16c7e448160"},
+]
